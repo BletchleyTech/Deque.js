71402992dfdd0380e3ebeecaaf39c1c81b64364b
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-17 18:19:54 -0500

    Compiled Deque.js from src/Deque.ts

--- Deque.js
+++ Deque.js
@@ -1,47 +1,59 @@
-module.exports = class Deque {
-    #deque;
-    constructor() {
-        this.#deque = [];
-    }
-    get front() {
-        return this.#getFront();
-    }
-    get back() {
-        return this.#getBack();
-    }
-    get size() {
-        return this.#deque.length;
-    }
-    get isEmpty() {
-        return this.#deque.length === 0;
-    }
-    get deque() {
-        return this.#deque.join(" - ");
-    }
-    enqueueFront(element) {
-        this.#deque.unshift(element);
-    }
-    enqueueBack(element) {
-        this.#deque.push(element);
-    }
-    enqueueMultipleFront(...elements) {
-        elements.flat().forEach(element => this.enqueueFront(element));
-    }
-    enqueueMultipleBack(...elements) {
-        elements.flat().forEach(element => this.enqueueBack(element));
-    }
-    dequeueFront() {
-        if (this.isEmpty) return "Underflow";
-        return this.#deque.shift();
-    }
-    dequeueBack() {
-        if (this.isEmpty) return "Underflow";
-        return this.#deque.pop();
-    }
-    #getFront() {
-        return this.#deque[0];
-    }
-    #getBack() {
-        return this.#deque[this.#deque.length - 1];
-    }
+export default class Deque {
+	#deque;
+	#type;
+	constructor(deque) {
+		this.#deque = deque ? deque.#deque : [];
+		this.#type = deque ? deque.#type : "";
+	}
+	get front() {
+		return this.#getFront();
+	}
+	get back() {
+		return this.#getBack();
+	}
+	get size() {
+		return this.#deque.length;
+	}
+	get isEmpty() {
+		return this.#deque.length === 0;
+	}
+	get deque() {
+		return this.#toString();
+	}
+	get type() {
+		return this.#type;
+	}
+	enqueueFront(element) {
+		if (this.isEmpty && !this.#type) this.#type = typeof element;
+		if (typeof element !== this.#type) throw new Error("Invalid type");
+		this.#deque.unshift(element);
+	}
+	enqueueBack(element) {
+		if (this.isEmpty && !this.#type) this.#type = typeof element;
+		if (typeof element !== this.#type) throw new Error("Invalid type");
+		this.#deque.push(element);
+	}
+	enqueueManyFront(...elements) {
+		elements.forEach((element) => this.enqueueFront(element));
+	}
+	enqueueManyBack(...elements) {
+		elements.forEach((element) => this.enqueueBack(element));
+	}
+	dequeueFront() {
+		if (this.isEmpty) throw new Error("Underflow");
+		return this.#deque.shift();
+	}
+	dequeueBack() {
+		if (this.isEmpty) throw new Error("Underflow");
+		return this.#deque.pop();
+	}
+	#getFront() {
+		return this.#deque[0];
+	}
+	#getBack() {
+		return this.#deque[this.#deque.length - 1];
+	}
+	#toString() {
+		return this.#deque.join(" - ");
+	}
 }
